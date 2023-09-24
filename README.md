8aa42aee77ffb71e87fe038f3e71072a64a51b15
madison reynolds <madison.reynolds@phoenix.edu>
2023-09-23 22:26:55 -0700

    added info and sources to Readme and cleaned up comments to make it easier to read

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # Third-Party-API-Scheduler
+
+I made changes to the starter code to create a calendar so people can easily see what day it is. They can also tell which items of their day are in the past, present and future. There is also a way to save their tasks for each hour of the day. 
+
+## Instructions 
+
+Add what task you have each hour of the day and then click the saved button so the data with retain and be there when you refresh the page.
+
+## User Story
+
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Acceptance Criteria
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours of 9am to 5pm
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## Mock Up
+
+## Notes
+
+Here are some websites I used to help me with my assignment: (I also got help from my instructor, TA and Tutor):
+
+https://www.youtube.com/watch?v=UbQc4vtf62A
+
+https://www.youtube.com/watch?v=M2ZdOTuudH0
+
+https://api.jquery.com/jquery.now/
+
+https://www.tutorialspoint.com/javascript/date_gethours.htm#:~:text=Javascript%20Date%20getHours()%20method,integer%20between%200%20and%2023.
+
+https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+
+https://learn.jquery.com/using-jquery-core/document-ready/
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function
+
+https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+https://www.w3schools.com/js/js_loop_for.asp
+
+https://www.shecodes.io/athena/27498-how-to-convert-unix-timestamp-to-date-and-time-in-javascript
+
+https://www.youtube.com/watch?v=OvRFRPkyd3M
+
+https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-1.php
+
+
+
--- script.js
+++ script.js
@@ -8,8 +8,6 @@ $( document ).ready(function() {
 
 var currentDateContainer = document.getElementById("currentDay");
 
-// var currentDate = Date.now(); 
-
 var currentDate = new Date();
 
 currentDateContainer.innerHTML = currentDate;
@@ -17,13 +15,7 @@ var currentHour = currentDate.getHours()
 console.log(currentHour);
 
 for (let i = 9; i < 18; i++) {
-/*
-For Steps when creating HTML in JS
-1. Target a parent
-2. Create an element
-3. Give the element the necessary data
-4.Add the new element to the parent
-*/
+
 var hour
 var meridian
 if (i > 12){
@@ -46,9 +38,8 @@ div.innerHTML = `<div class="col-2 col-md-1 hour text-center py-3">` + hour + me
 <button class="btn saveBtn col-2 col-md-1" aria-label="save">
   <i class="fas fa-save" aria-hidden="true"></i>
 </button>`
-// timeblockHour < currentHOur past
-// TBH === CH present
-// TBH > CH future
+
+
 if (i < currentHour){
   div.classList.add('past');
 }else if (i === currentHour){
@@ -56,11 +47,12 @@ if (i < currentHour){
 }else {
   div.classList.add('future');
 }
-// parent.append(new element)
+
 container.append(div);
 
 } 
-// document.querySelector('.saveBtn').addEventListener(click, function (){})
+
+
 $('.saveBtn').on('click', function (){
   console.log('do you work');
   var key = $(this).parent().attr('id');
@@ -78,40 +70,6 @@ $('#hour-14 .description').val(localStorage.getItem('hour-14'));
 $('#hour-15 .description').val(localStorage.getItem('hour-15'));
 $('#hour-16 .description').val(localStorage.getItem('hour-16'));
 $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-
-
-
-// When the page loads, I want to grab the hours from local Storage and add it to it's respective timeblock value
-
-
-// const saveBtn = document.querySelector('btn saveBtn col-2 col-md-1');
-
-// $(function () {
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-
-    // saveBtn.addEventListener('click', )
-
-
-
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  // });
 
 });
 
